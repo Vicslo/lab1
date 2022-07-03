e79a84306ec84574d086fbecacc32af703fb2921
Vicslo <108408174+Vicslo@users.noreply.github.com>
2022-07-03 11:14:00 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<form  action="first.php" method="GET">
+<input type="submit" value="1" >
+</form>
+</form>
+</body>
+</html>
